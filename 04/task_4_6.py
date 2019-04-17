8dadf4bde31572d7a1cb8ce41ace615ed426af2e
TarasKindrat <kindrat5@gmail.com>
2019-04-17 15:31:30 +0000

    4.6 strip ',' in othe plase

--- 04/task_4_6.py
+++ 04/task_4_6.py
@@ -18,10 +18,10 @@ ospf_route = 'OSPF    10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/
 
 list_ospf = ospf_route.strip().split()
 list_ospf.remove('via')
-list2_ospf = [element.strip('[').strip(']').strip(',') for element in list_ospf]
+list2_ospf = [element.strip('[').strip(']') for element in list_ospf]
 print (list2_ospf)
 
 keys = ['Protocol', 'Prefix', 'AD/Metric', 'Next-Hop', 'Last update', 'Outbound Interface']
 
 for i in range(len(keys)):
-   print('{:<20} {}'.format(keys[i],list_ospf[i]))
+   print('{:<20} {}'.format(keys[i],list_ospf[i].strip(',')))
